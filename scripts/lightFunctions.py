03ea0e659d60b93e1582583d96ea3d6784b31980
RudeCow <91128783+RudeCow@users.noreply.github.com>
2021-11-16 02:33:05 +0300

    Update lightFunctions.py

--- scripts/lightFunctions.py
+++ scripts/lightFunctions.py
@@ -1,19 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import imageio
+import imageio 
+from numpy.core.fromnumeric import size
 
 def readIntensity(photoName, plotName, lamp, surface):
     photo = imageio.imread(photoName)
     cut = photo[400:1050, 1130:1290, 0:3].swapaxes(0, 1)
-    intensity = np.mean(cut, axis=(2, 0))
+    rgb = np.mean(cut, axis=(0))
+    luma = 0.2989 * rgb[:, 0] + 0.5866 * rgb[:, 1] + 0.1144 * rgb[:, 2]
 
-    fig = plt.figure(figsize=(10, 3), dpi=200)
-    plt.plot(intensity, 'w', label='{} / {}'.format(lamp, surface))
+    fig = plt.figure(figsize=(11, 10), dpi=200)
+    plt.plot(luma, 'w', label='{} / {}'.format(lamp, surface))
     plt.imshow(cut, origin='lower')
     plt.title('Интенсивность отражённого излучения')
     plt.xlabel('Относительный номер пикселя')
     plt.ylabel('Яркость')
     plt.legend()
-    plt.savefig(plotName)
+    plt.savefig(plotName) 
+    return luma
 
-    return intensity
+
